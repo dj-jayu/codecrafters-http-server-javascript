5dc4d7a0b087ff08f2cac4bd24e7c8cf81ff9a46
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-14 17:39:49 -0300

    pass 2nd stage #first try

--- app/main.js
+++ app/main.js
@@ -5,16 +5,21 @@ console.log("Logs from your program will appear here!");
 
 const server = net.createServer(socket => {
     socket.on('close', () => {
-        console.log('connection closed')
+        console.log('connection closed');
         socket.end();
         server.close();
     });
 });
 
 server.on('connection', (stream) => {
-    console.log('someone connected!')
+    console.log('someone connected!');
+    stream.on('data', buffer => {
+        console.log(buffer.toString());
+        stream.write('HTTP/1.1 200 OK\r\n\r\n');
+        stream.end();
+    })
 })
 
 server.listen(4221, "localhost", () => {
-    console.log('listening for new connections')
+    console.log('listening for new connections');
 })
