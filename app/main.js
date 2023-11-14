2034287f7cf74a09c195f7365fee6ee2b1081ef6
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-14 16:20:48 -0300

    pass 1st stage

--- app/main.js
+++ app/main.js
@@ -3,12 +3,18 @@ const net = require("net");
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this to pass the first stage
-// const server = net.createServer((socket) => {
-//   socket.on("close", () => {
-//     socket.end();
-//     server.close();
-//   });
-// });
-//
-// server.listen(4221, "localhost");
+const server = net.createServer(socket => {
+    socket.on('close', () => {
+        console.log('connection closed')
+        socket.end();
+        server.close();
+    });
+});
+
+server.on('connection', (stream) => {
+    console.log('someone connected!')
+})
+
+server.listen(4221, "localhost", () => {
+    console.log('listening for new connections')
+})
