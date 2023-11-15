7c7f09afddb41c2f5fec955f79232b4fd78a11d3
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-15 02:00:00 -0300

    pass stage 7th #first try

--- app/main.js
+++ app/main.js
@@ -1,9 +1,31 @@
 const net = require("net");
-
+const fs = require('fs');
+const path = require('path');
 const END_LINE = '\r\n';
 const END_HEADERS = END_LINE + END_LINE
+const DIRECTORY = getDirectoryFromArgs();
+
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
+
+function getDirectoryFromArgs() {
+    for (let i = 0; i < process.argv.length; i++) {
+        if (process.argv[i] === '--directory' && i < process.argv.length - 1) {
+            return process.argv[i+1];
+        }
+    }
+    return '';
+}
+
+async function getFileContent(file_name) {
+    const full_path = path.join(DIRECTORY, file_name);
+    let returnArr = [];
+    const data = await fs.promises.readFile(full_path);
+    // to test the non blocking behavior of Node uncomment the line below
+    // and try other requests while awaiting for the file
+    // const delay = await new Promise(resolve => setTimeout(resolve, 10000));
+    return data;
+}
 
 function parseResponse(response) {
     // returns the headers and body content from the raw client response
@@ -34,19 +56,27 @@ function createServerResponse(statusCode, statusMsg, headersDict={}, body='') {
     return request_line + END_LINE + headers + END_HEADERS + body;
 }
 
-function processResponse(response) {
+async function processResponse(response) {
     // deals with the logic of what the server should answer to the client
     const [headers, body] = parseResponse(response);
     if (headers['path'] === '/') {
         return createServerResponse(200, 'OK');
     } else if (headers['path'].startsWith('/echo')) {
-        serverResponseBody = headers['path'].slice(6);
+        serverResponseBody = headers['path'].slice(6).replace(/^\/+/, '');
         headersDict = {'Content-Type':'text/plain', 'Content-Length': serverResponseBody.length};
         return createServerResponse(200, 'OK', headersDict, serverResponseBody);
     } else if (headers['path'].startsWith('/user-agent')) {
         serverResponseBody = headers['user-agent'];
         headersDict = {'Content-Type':'text/plain', 'Content-Length': serverResponseBody.length};
         return createServerResponse(200, 'OK', headersDict, serverResponseBody);
+    } else if (headers['path'].startsWith('/file')) {
+        const file_name = headers['path'].slice(6).replace(/^\/+/, '');
+        try {
+            const content = await getFileContent(file_name);
+            return createServerResponse(200, 'OK', {'Content-Type': 'application/octect-stream', 'Content-Length': content.length}, content);
+        } catch {
+            return createServerResponse(404, 'Not Found');
+        }
     }
     else {
         return createServerResponse(404, 'Not Found');
@@ -64,9 +94,9 @@ const server = net.createServer(socket => {
 // defines what the server does when there's a connection
 server.on('connection', (stream) => {
     console.log(`Client ${stream.remoteAddress} connected`);
-    stream.on('data', buffer => {
+    stream.on('data', async buffer => {
         const response = buffer.toString();
-        const data_for_client = processResponse(response)
+        const data_for_client = await processResponse(response);
         stream.write(data_for_client);
         stream.end();
     })
